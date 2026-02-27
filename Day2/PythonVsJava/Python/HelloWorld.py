8c65a39fb18491b93af37747c6f8f2571576074b
user.email="deepakbajaj79@gmail.com <deepakbajaj79@gmail.com>
2026-02-27 16:48:47 +0530

    Till Control  Variables and Types are done

---
+++ Day2/PythonVsJava/Python/HelloWorld.py
@@ -0,0 +1,65 @@
+print("Hello World")
+
+if 9>10:
+    print("9 is Greater")
+else:
+    print("10 is Greater")
+
+for i in range(10):
+    print(i)
+
+
+##### Variables and Types ###
+a=10
+b=10
+
+print(a)
+print(b)
+
+## Python Data Types
+'''
+Boolean True, False
+Integer 10
+Float
+String
+
+'''
+a="Deepak"
+b="Anuj"
+
+print(a)
+print(b)
+
+a=True
+b=False
+
+print(a)
+print(b)
+
+a=10.5
+b=14.5
+
+print(a)
+print(b)
+
+PI = 3.45
+print(PI)
+PI="D"
+print(PI)
+
+print(type(10))
+print(isinstance(10,int))
+print(isinstance(True,bool))
+
+print(isinstance(10,int))
+print(isinstance(True,int))
+
+x = int(input("Enter a Number 1"))
+y = int(input ("Enter a  Second Number 2"))
+
+z = x+y
+
+print(z)
+
+
+
